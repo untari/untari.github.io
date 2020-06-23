c981f4a507c066da3f472d6b1249c5ac3aa0f2a4
untari <sastraembun0306@gmail.com>
2020-06-23 22:20:21 +0700

    adding animation

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,8 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Bulb2 from './bulb2.svg';
 
-const Home = () => {
-    return (
-       <div><h2>Home</h2></div>
-    );
+const docBody = document.querySelector ('body');
+
+class Home extends Component {
+        constructor(props) {
+            super(props);
+                this.state = {bgColored: false};
+                this.colorBackground = this.colorBackground.bind(this);
+            }
+            colorBackground() {
+                this.setState({bgColored: true})
+            }
+            componentDidUpdate(preProps, prevState) {
+                const { bgColored } = this.state;
+                const className = 'whiteBg';
+                
+                if(prevState.bgColored !== bgColored){
+                    bgColored ?
+                        docBody.classList.add(className) :
+                        docBody.classList.remove(className);
+                }
+                    
+      }
+      render() {
+        return(
+            <div className="centered">
+                <a href="#" onClick={() => this.colorBackground()}><img src={Bulb2} alt="on" /></a>
+            </div>
+        );
+        }
 }
 export default Home;
--- src/Components/Main.js
+++ src/Components/Main.js
@@ -3,28 +3,23 @@ import { Route, Link } from "react-router-dom";
 import Home from './Home';
 import About from './About';
 import Contact from './Contact';
-import Cover from './Cover';
-
 
 class Main extends Component {
        render() {
-          return(
-            <div>
-               <div className="centered">
-                 <Cover />
-               </div>
-               <ul>
-                 <li><Link to="/">Home</Link></li>
-                 <li><Link to="/about">About</Link></li>
-                 <li><Link to="/contact">Contact</Link></li>
-               </ul>
-               <hr />
+            return(
+                <div>
+                    <ul>
+                        <li><Link to="/">Home</Link></li>
+                        <li><Link to="/about">About</Link></li>
+                        <li><Link to="/contact">Contact</Link></li>
+                    </ul>
+                    <hr />
 
-               <Route exact path="/" component={Home} />
-               <Route path="/about" component={About} />
-               <Route path="/contact" component={Contact} />
-            </div>
-          );
-   }
+                    <Route exact path="/" component={Home} />
+                    <Route path="/about" component={About} />
+                    <Route path="/contact" component={Contact} />
+                </div>
+            );
+    }
 }
 export default Main;
