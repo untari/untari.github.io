4be6fc87acd935357de21ce0e2adc7c1c4716845
untari <sastraembun0306@gmail.com>
2020-07-03 23:29:09 +0700

    fixed clickable navbar

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -17,11 +17,11 @@ class Main extends Component {
                 <a href="#" className="navbar-brand">
                     <img src={Logo} alt="logo" heigth="40" width="40" /><span className="text-dark">untari.com</span>
                 </a>
-                <button type="button" className="navbar-toggler" data-toggle="collapse" data-target=".navbar-collapse">
+                <button type="button" className="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
                     <span className="navbar-toggler-icon"></span>
                 </button>
 
-                <div className="navbar-collapse collapse ">
+                <div className="collapse navbar-collapse" id="navbarCollapse">
                     <div className="navbar-nav ml-auto">
                         <a href="#home" className="nav-item nav-link active">Home</a>
                         <a href="#about" className="nav-item nav-link active">About me</a>
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.js'
 import 'font-awesome/css/font-awesome.css';
 import './index.css';
 import App from './App';
