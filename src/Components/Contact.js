39a0a5d60c9dc91e2e79cbce0547f646a3b3f912
untari <sastraembun0306@gmail.com>
2020-07-13 21:38:40 +0700

    Sending Emails With EmailJS

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,79 +1,97 @@
 import React, { Component } from 'react';
-import { Button, Form, FormGroup, Label,  Input, FormFeedback, FormText } from 'reactstrap';
+import * as emailjs from 'emailjs-com';
+import { Button, FormFeedback, Form, FormGroup, Label, Input } from 'reactstrap';
 
-class  Contact extends Component {
-    constructor(props) {
-        super(props);
-          this.state = {
-          'email': '',
-          validate: {
-            emailState: '',
-          },
-        }
-        this.handleChange = this.handleChange.bind(this);
+class ContactForm extends Component {
+  state = {
+    name: '',
+    email: '',
+    subject: '',
+    message: '',
+  }
+  handleSubmit(e) {
+    e.preventDefault()
+    const { name, email, subject, message } = this.state
+    let templateParams = {
+      from_name: this.state.name + " (" + this.state.email + ")",
+      to_name: 'sastraembun0306@gmail.com',
+      subject: subject,
+      message_html: message,
     }
-
-    validateEmail(e) {
-        const emailRex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-        const { validate } = this.state
-          if (emailRex.test(e.target.value)) {
-            validate.emailState = 'has-success'
-          } else {
-            validate.emailState = 'has-danger'
-          }
-          this.setState({ validate })
-    }
-
-    handleChange = async (event) => {
-        const { target } = event;
-        const value = target.type === 'checkbox' ? target.checked : target.value;
-        const { name } = target;
-        await this.setState({
-          [ name ]: value,
-        });
-    }
-    handleSubmit = (e) => {
-        e.preventDefault();
-        alert("Congrats, you submitted your message!");
-    }
-
-    render() {
-        const { email } = this.state;
-        return (
-           <div>
-             <p>Drop me message here!</p>
-             <Form className="form" onSubmit={this.handleSubmit}>
-                <FormGroup className="col-lg-3 col-md-3 col-sm-3 col-xs-3">
-                    <Label>Name</Label>
-                    <Input type="name" name="name" id="name"  />
-                </FormGroup>
-                <FormGroup className="col-lg-3 col-md-3 col-sm-3 col-xs-3">
-                   <Label>Email</Label>
-                   <Input type="email" name="email" id="email"
-                     value={ email }
-                     valid={ this.state.validate.emailState === 'has-success' }
-                     invalid={ this.state.validate.emailState === 'has-danger' }
-                     onChange={ (e) => {
-                            this.validateEmail(e)
-                            this.handleChange(e)
-                            } }
-                   />
-                   <FormFeedback valid>
-                        That's a tasty looking email you've got there.
-                   </FormFeedback>
-                   <FormFeedback>
-                        Uh oh! Looks like there is an issue with your email. Please input a correct email.
-                   </FormFeedback>
-                   <FormText>Your username is most likely your email.</FormText>
-                </FormGroup>
-                <FormGroup className="col-lg-3 col-md-3 col-sm-3 col-xs-3">
-                   <Label>Text Area</Label>
-                   <Input type="textarea" name="text" id="text" />
-                </FormGroup>
-                <Button>Send</Button>
-             </Form>
-           </div>
-        );
-    }
-}
-export default Contact;
+    emailjs.send(
+      'gmail',
+      'template_vZoBYmUO',
+       templateParams,
+      'user_vAG2BmDDIX1solSFjQEmf'
+    )
+    this.resetForm()
+ }
+ resetForm() {
+    this.setState({
+      name: '',
+      email: '',
+      subject: '',
+      message: '',
+    })
+ }
+ handleChange = (param, e) => {
+    this.setState({ [param]: e.target.value })
+ }
+ render() {
+    return (
+      <>
+        <div>
+          <h1 className="p-heading1">Get in Touch</h1>
+          <Form onSubmit={this.handleSubmit.bind(this)}>
+            <FormGroup controlId="formBasicEmail" className="col-lg-3 col-md-3 col-sm-3 col-xs-3">
+              <Label className="text-muted">Email address</Label>
+              <Input
+                type="email"
+                name="email"
+                value={this.state.email}
+                className="text-primary"
+                onChange={this.handleChange.bind(this, 'email')}
+                placeholder="Enter email"
+              />
+            </FormGroup>
+            <FormGroup controlId="formBasicName" className="col-lg-3 col-md-3 col-sm-3 col-xs-3">
+              <Label className="text-muted">Name</Label>
+              <Input
+                type="text"
+                name="name"
+                value={this.state.name}
+                className="text-primary"
+                onChange={this.handleChange.bind(this, 'name')}
+                placeholder="Name"
+              />
+            </FormGroup>
+            <FormGroup controlId="formBasicSubject" className="col-lg-3 col-md-3 col-sm-3 col-xs-3">
+              <Label className="text-muted">Subject</Label>
+              <Input
+                type="text"
+                name="subject"
+                className="text-primary"
+                value={this.state.subject}
+                onChange={this.handleChange.bind(this, 'subject')}
+                placeholder="Subject"
+              />
+            </FormGroup>
+            <FormGroup controlId="formBasicMessage" className="col-lg-3 col-md-3 col-sm-3 col-xs-3">
+              <Label className="text-muted">Message</Label>
+              <Input
+                type="textarea"
+                name="message"
+                className="text-primary"
+                value={this.state.message}
+                onChange={this.handleChange.bind(this, 'message')}
+              />
+            </FormGroup>
+            <Button variant="primary" type="submit">
+              Submit
+            </Button>
+          </Form>
+        </div>
+      </>
+    )
+  }
+}export default ContactForm
