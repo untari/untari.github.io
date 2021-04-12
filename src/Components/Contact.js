7a637afe6038d18667b626b12d797f404d2843a8
untari <sastraembun0306@gmail.com>
2021-04-12 14:35:46 +0700

    modified contact form and adding validation

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,123 +1,66 @@
-import React from 'react'
-import * as emailjs from 'emailjs-com';
-import { Button, FormGroup, Label, Input } from "reactstrap";
-import { Form, Field } from "react-final-form";
+import React from "react";
+import { useForm } from "react-hook-form";
 
-const onSubmit = values => {
-  console.log(values, sendEmail);
-  alert("Congrats, you submitted your message!");
 
-    
-    function sendEmail(e) {
-        e.preventDefault();
-        
-        emailjs.sendForm('service_oe82hof', 'template_vZoBYmUO', e.target, 'user_vAG2BmDDIX1solSFjQEmf')
-        .then((result) => {
-            console.log(result.text);
-        }, (error) => {
-            console.log(error.text);
-        });
-    }
-}
-const ContactForm = () => (
-  <Form
-    onSubmit={onSubmit}
-    
-    validate={values => {
-      const errors = {};
-      function validateEmail(email) {
-        var re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-        return re.test(String(email).toLowerCase());
-      }
-      if (!values.fname) {
-        errors.fname = "Required";
-      }
+const Contact = () => {
+  const { register, handleSubmit, errors } = useForm();
 
-      if (!values.email) {
-        errors.email = "Required";
-      } else if (!validateEmail(values.email)) {
-        errors.email = "Not an email adress";
-      }
-      if (!values.message) {
-        errors.message = "Required";
-      }
-      return errors;
-      
-    }}
-    render={({ handleSubmit, values, submitting, validating, valid }) => (
-        <div class="container">
-            <div class="contact">
-                <h2 class="page-section-heading text-center text-uppercase text-secondary mb-0">Contact Me</h2>
-                    <div class="divider-custom">
-                        <div class="divider-custom-line"></div>
-                        <div class="divider-custom-icon"><i class="fa fa-star"></i></div>
-                        <div class="divider-custom-line"></div>
-                    </div>
-                    <div class="row">
-                        <div class="col-lg-6 mx-auto">
-                            <form onSubmit={handleSubmit}>
-                                <FormGroup>
-                                    <Label for="fname"> Name</Label>
-                                    <Field name="fname">
-                                        {({ input, meta }) => (
-                                        <div>
-                                            <Input
-                                            {...input}
-                                            type="text"
-                                            placeholder="Name"
-                                            
-                                            invalid={meta.error && meta.touched}
-                                            />
-                                            {meta.error && meta.touched && <span>{meta.error}</span>}
-                                        </div>
-                                        )}
-                                    </Field>
-                                </FormGroup>
-                                <FormGroup>
-                                    <Label for="email">Email</Label>
-                                    <Field name="email">
-                                        {({ input, meta }) => (
-                                        <div>
-                                            <Input
-                                            {...input}
-                                            type="text"
-                                            placeholder="Enter Email"
-                                    
-                                            invalid={meta.error && meta.touched}
-                                            />
-                                            {meta.error && meta.touched && <span>{meta.error}</span>}
-                                        </div>
-                                        )}
-                                    </Field>
-                                </FormGroup>
-                                <FormGroup>
-                                    <Label for="message">Message</Label>
-                                    <Field name="message">
-                                        {({ input, meta }) => (
-                                        <div>
-                                            <Input
-                                            {...input}
-                                            type="textarea"
-                                            placeholder="Enter Message"
-                                            
-                                            invalid={meta.error && meta.touched}
-                                            />
-                                            {meta.error && meta.touched && <span>{meta.error}</span>}
-                                        </div>
-                                        )}
-                                    </Field>
-                                </FormGroup>
+  function onSubmit(data) {
+    console.log("Data submitted: ", data);
+  }
 
-                                <Button type="submit" color="info" >
-                                    Send
-                                </Button>
-                            </form>
-                        </div>
-                    </div>
-                 </div>
+  return (
+    <div className="container">
+        <div className="contact">
+            <h1 className="page-section-heading text-center text-uppercase text-secondary mb-0">Get in Touch</h1>
+            <div className="divider-custom">
+                <div className="divider-custom-line"></div>
+                <div className="divider-custom-icon"><i class="fa fa-star"></i></div>
+                <div className="divider-custom-line"></div>
             </div>
-        )}
-    />
-);
+            <div className="row">
+                <div className="login-form">
+                     <form onSubmit={handleSubmit(onSubmit)} noValidate>
+                            <label htmlFor="inputEmail">E-mail</label>
+                            <input
+                            type="email"
+                            id="inputEmail"
+                            name="email"
+                            ref={register({
+                                required: "Enter your e-mail",
+                                pattern: {
+                                value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+                                message: "Enter a valid e-mail address",
+                                },
+                            })}
+                            />
+                            {errors.email && <p className="error">{errors.email.message}</p>}
 
-export default ContactForm;
+                            <label htmlFor="inputName">Name</label>
+                            <input
+                            type="text"
+                            id="inputName"
+                            name="name"
+                            ref={register({ required: "Enter your name" })}
+                            />
+                            {errors.name && <p className="error">{errors.name.message}</p>}
+                            
+                            <label htmlFor="inputTextarea"> Message </label>
+                            <input
+                            type="textarea"
+                            id="inputTextarea"
+                            name="textarea"
+                            ref={register({ required: "Enter your message" })}
+                            />
+                            {errors.textarea && <p className="error">{errors.textarea.message}</p>}
+
+                            <button type="submit" color="info">Submit</button>
+                        </form>
+                </div>
+            </div>
+        </div>
+    </div>
+  );
+};
+
+export default Contact;
