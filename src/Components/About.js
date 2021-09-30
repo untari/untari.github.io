8a441dacf99b50e04226ee46f636fbe8b076ba69
untari <sastraembun0306@gmail.com>
2021-09-30 19:22:32 +0700

    add more text in about me page

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,30 +1,29 @@
 import React, { Component } from 'react';
 import Tari from '../Images/tari.png';
-import {  Card, CardText, CardBody, CardImg } from 'reactstrap';
 
 
 class About extends Component {
   render() {
     return (
-             <div  className="container"> 
-                 <div className="about">
-                            <Card style={{border: "none", backgroundColor:"#c7ebea"}}>
-                                <CardImg
-                                src={Tari}
-                                alt="avatar"
-                                className="img-fluid"
-                                style={{ borderRadius: "25px", width: "30%"}}
-                                />
-                                <CardBody>
-                                <h3>Untari</h3>
-                                <CardText>
-                                    <p> I am a frontend software developer looking for challenging projects that<br /> make an impact and allow me to keep learning and improving my skills.
-                                      <p/>
-                                    </CardText>
-                                </CardBody>
-                            </Card>
+        <div className="card" >
+            <div className="row no-gutters about">
+                <div className="col-md-4">
+                    <img src={Tari} className="card-img" alt="card-img" />
+                </div>
+                <div className="col-md-8">
+                    <div className="card-body">
+                        <h5 className="card-title">Tari, it's my name!</h5>
+                        <p className="card-text" >I am a student of Information Systems at the College of Management and Computer Science, GICI, Batam, Indonesia. I am also a front-end developer that likes to
+                        create fun, innovative, accessible and fast websites. I try to leave every bit of code I touch morereadable, modular, efficient and accessible than I found it. <br />I am also an open source enthusiast, since I believe it has multiple benefits for society, including
+                        myself, since I can learn and evolve with it. I like taking part in open source communities as it
+                        helps me learn from others, improve my coding skills and make me a better communicator. So
+                        far I have attended several open source talks and events and collaborated with the FOSSASIA
+                        and AnitaB organizations.
+                        </p>
                     </div>
+                </div>
             </div>
+        </div>
     );
   }
 }
