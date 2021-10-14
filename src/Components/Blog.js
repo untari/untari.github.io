fa03d956e445cfd0657dc49ccd7e7d2ed897a208
untari <sastraembun0306@gmail.com>
2021-10-14 19:04:59 +0700

    adjust the padding in blog page layout & change background color

--- src/Components/Blog.js
+++ src/Components/Blog.js
@@ -11,14 +11,14 @@ class Blog extends React.Component {
                 </div>
                 <div className="row">
                     <div className="leftcolumn">
-                        <div className="card">
+                        <div className="card" id="blog">
                             <h2>Introduction</h2>
                             <h5>It's all about my personal and web development journey, Oct 14, 2021</h5>
                             <div className="fakeimg" /*style="height:200px;"*/>Image</div>
                             <p>Some text..</p>
                             <p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
                         </div>
-                        <div className="card">
+                        <div className="card" id="blog">
                             <h2>First Week as UoL Student</h2>
                             <h5>Daily routine as student in UoL, Oct 14 2, 2021</h5>
                             <div className="fakeimg" /*style="height:200px;"*/>Image</div>
@@ -27,20 +27,16 @@ class Blog extends React.Component {
                         </div>
                     </div>
                     <div className="rightcolumn">
-                        <div className="card">
+                        <div className="card" id="blog">
                             <h2>About Me</h2>
                         <div className="fakeimg" /*style="height:100px;"*/>Image</div>
                             <p>Some text about me in culpa qui officia deserunt mollit anim..</p>
                         </div>
-                        <div className="card">
+                        <div className="card" id="blog">
                             <h3>Popular Post</h3>
                             <div className="fakeimg">Image</div><br />
                             <div className="fakeimg">Image</div><br />
                             <div className="fakeimg">Image</div>
-                        </div>
-                        <div className="card">
-                            <h3>Follow Me</h3>
-                            <p>Some text..</p>
                         </div>
                     </div>
                 </div>
