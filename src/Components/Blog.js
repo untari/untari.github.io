7dc288a122085f3ff18015c9be1b830747cfa0f9
untari <sastraembun0306@gmail.com>
2022-09-21 14:22:14 +0800

    writing a content for a blog

--- src/Components/Blog.js
+++ src/Components/Blog.js
@@ -1,4 +1,5 @@
 import React from "react";
+import pic1 from "../Images/blog1.jpeg";
 import "../App.css";
 
 
@@ -13,15 +14,19 @@ class Blog extends React.Component {
                     <div className="leftcolumn">
                         <div className="card" id="blog">
                             <h2>Introduction</h2>
-                            <h5>It's all about my personal and web development journey, Oct 14, 2021</h5>
-                            <div className="fakeimg" /*style="height:200px;"*/>Image</div>
-                            <p>Some text..</p>
+                            <h5>It's all about my web development journey, Oct 14, 2021</h5>
+                            <img className="img-thumbnail" width={300} src={pic1}/>
+                            <p>I started learning web development back in 2019. First, I wasn't sure where shall I begin as there are so many programming language to choose or things what to focus on.<br />
+
+
+            \
+                            </p>
                             <p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
                         </div>
                         <div className="card" id="blog">
                             <h2>First Week as UoL Student</h2>
                             <h5>Daily routine as student in UoL, Oct 14 2, 2021</h5>
-                            <div className="fakeimg" /*style="height:200px;"*/>Image</div>
+                            <div className="fakeimg" src={pic1} >Image</div>
                             <p>Some text..</p>
                             <p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur.</p>
                         </div>
