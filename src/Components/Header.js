072ec7010c86bd022188bd64d122c6810da76790
Tari <untari.dev@gmail.com>
2025-09-29 20:44:33 +0800

    fix the header and footer icons

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -19,7 +19,7 @@ class Header extends Component {
             <div>
                 <div>
                     <a href="https://github.com/untari/untari.github.io"> 
-                        <img loading="lazy" width="149" height="149" src="https://github.blog/wp-content/uploads/2008/12/forkme_right_darkblue_121621.png?resize=149%2C149" className="attachment-full size-full float-right" alt="Fork me on GitHub" data-recalc-dims="1"/> 
+                        <img loading="lazy" width="149" height="149" src="https://github.blog/wp-content/uploads/2008/12/forkme_right_darkblue_121621.png?resize=149%2C149" className="attachment-full size-full float-end" alt="Fork me on GitHub" data-recalc-dims="1"/> 
                     </a>
                 </div>
                 <nav className="navbar navbar-expand-lg navbar-dark fixed-top ">
